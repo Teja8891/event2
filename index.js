efbe77503adfea57394e0ee4104be90aa381006e
Teja8891 <102683189+Teja8891@users.noreply.github.com>
2022-08-20 21:57:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  const handleClick = (event) =>{
+    console.log('Button id is:-'+event.target.id);
+  }
+
+  // do not remove the two buttons or change their id
+  return (
+    <div id="main">
+      <button onClick={handleClick} id="button-a">Button A</button>
+      <button onClick={handleClick} id="button-b">Button B</button>
+    </div>
+  )
+}
+
+
+export default App;
